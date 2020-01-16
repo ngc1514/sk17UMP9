bb62e43626752448829f7a57757e0b5b3e4cc761
Shesky17 <aidenx58@gmail.com>
2020-01-16 02:37:44 -0500

    Reduced draw time. Need to fix automatic firing and animation. Also prolly key bindings.

--- src/main/java/com/shesky17/sk17ump9/guns/GunBase.java
+++ src/main/java/com/shesky17/sk17ump9/guns/GunBase.java
@@ -6,6 +6,7 @@ import com.shesky17.sk17ump9.init.ModBullet;
 import com.shesky17.sk17ump9.init.ModGuns;
 import com.shesky17.sk17ump9.util.IHasModel;
 import com.shesky17.sk17ump9.util.Ref;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -20,6 +21,7 @@ import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
@@ -50,16 +52,10 @@ public class GunBase extends ItemBow implements IHasModel
     }
 
     @Override
-    public int getItemEnchantability() {
-        return 10;
-    }
-
+    public int getItemEnchantability() { return 10; }
 
     @Override
-    public int getMaxItemUseDuration(ItemStack stack) {
-        return 10;
-    }
-
+    public int getMaxItemUseDuration(ItemStack stack) { return 5; }
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
@@ -67,15 +63,15 @@ public class GunBase extends ItemBow implements IHasModel
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 
-    //FIXME: goal is to reduce the bow draw time
     @Override
+    //FIXME: i dont want it to stop when holding, need to fix it
     public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entityLiving, int timeLeft)
     {
         if (entityLiving instanceof EntityPlayer)
         {
             EntityPlayer entityplayer = (EntityPlayer)entityLiving;
             boolean flag = entityplayer.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, stack) > 0;
-            ItemStack itemstack = this.findAmmo(entityplayer);
+            ItemStack itemstack = this.findAmmo2(entityplayer);
 
             int i = this.getMaxItemUseDuration(stack) - timeLeft;
             i = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(stack, worldIn, entityplayer, i, !itemstack.isEmpty() || flag);
@@ -83,65 +79,47 @@ public class GunBase extends ItemBow implements IHasModel
 
             if (!itemstack.isEmpty() || flag)
             {
-                if (itemstack.isEmpty())
-                {
+                if (itemstack.isEmpty()) {
                     itemstack = new ItemStack(Items.ARROW);
                 }
 
-                float f = getArrowVelocity(i);
-
-                if ((double)f >= 0.1D)
-                {
+                float f = getArrowVelocity2();
+                if ((double)f >= 0.1D) {
                     boolean flag1 = entityplayer.capabilities.isCreativeMode || (itemstack.getItem() instanceof ItemArrow && ((ItemArrow) itemstack.getItem()).isInfinite(itemstack, stack, entityplayer));
 
-                    if (!worldIn.isRemote)
-                    {
+                    if (!worldIn.isRemote) {
                         ItemArrow itemarrow = (ItemArrow)(itemstack.getItem() instanceof ItemArrow ? itemstack.getItem() : Items.ARROW);
                         EntityArrow entityarrow = itemarrow.createArrow(worldIn, itemstack, entityplayer);
                         entityarrow.shoot(entityplayer, entityplayer.rotationPitch, entityplayer.rotationYaw, 0.0F, f * 3.0F, 1.0F);
 
-                        if (f == 1.0F)
-                        {
+                        if (f == 1.0F) {
                             entityarrow.setIsCritical(true);
                         }
 
                         int j = EnchantmentHelper.getEnchantmentLevel(Enchantments.POWER, stack);
-
-                        if (j > 0)
-                        {
+                        if (j > 0) {
                             entityarrow.setDamage(entityarrow.getDamage() + (double)j * 0.5D + 0.5D);
                         }
 
                         int k = EnchantmentHelper.getEnchantmentLevel(Enchantments.PUNCH, stack);
-
-                        if (k > 0)
-                        {
+                        if (k > 0) {
                             entityarrow.setKnockbackStrength(k);
                         }
-
-                        if (EnchantmentHelper.getEnchantmentLevel(Enchantments.FLAME, stack) > 0)
-                        {
+                        if (EnchantmentHelper.getEnchantmentLevel(Enchantments.FLAME, stack) > 0) {
                             entityarrow.setFire(100);
                         }
 
                         stack.damageItem(1, entityplayer);
-
-                        if (flag1 || entityplayer.capabilities.isCreativeMode && (itemstack.getItem() == Items.SPECTRAL_ARROW || itemstack.getItem() == Items.TIPPED_ARROW))
-                        {
+                        if (flag1 || entityplayer.capabilities.isCreativeMode && (itemstack.getItem() == Items.SPECTRAL_ARROW || itemstack.getItem() == Items.TIPPED_ARROW)) {
                             entityarrow.pickupStatus = EntityArrow.PickupStatus.CREATIVE_ONLY;
                         }
-
                         worldIn.spawnEntity(entityarrow);
                     }
 
                     worldIn.playSound((EntityPlayer)null, entityplayer.posX, entityplayer.posY, entityplayer.posZ, SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
-
-                    if (!flag1 && !entityplayer.capabilities.isCreativeMode)
-                    {
+                    if (!flag1 && !entityplayer.capabilities.isCreativeMode) {
                         itemstack.shrink(1);
-
-                        if (itemstack.isEmpty())
-                        {
+                        if (itemstack.isEmpty()) {
                             entityplayer.inventory.deleteStack(itemstack);
                         }
                     }
@@ -152,19 +130,51 @@ public class GunBase extends ItemBow implements IHasModel
         }
     }
 
+    //because findAmmo is private, copying makes life easier....
+    private ItemStack findAmmo2(EntityPlayer player)
+    {
+        if (this.isArrow(player.getHeldItem(EnumHand.OFF_HAND))) {
+            return player.getHeldItem(EnumHand.OFF_HAND);
+        }
+        else if (this.isArrow(player.getHeldItem(EnumHand.MAIN_HAND))) {
+            return player.getHeldItem(EnumHand.MAIN_HAND);
+        }
+        else {
+            for (int i = 0; i < player.inventory.getSizeInventory(); ++i) {
+                ItemStack itemstack = player.inventory.getStackInSlot(i);
+                if (this.isArrow(itemstack)) {
+                    return itemstack;
+                }
+            }
+            return ItemStack.EMPTY;
+        }
+    }
+
+    //same reason, i copy this cuz i can't override it
+    //i mean, standard 9mm has a muzzle speed of 380m/s, one block = 1m, so....
+    public static float getArrowVelocity2() {return 380;}
+
+    //a custom method for firing using left key
+    public ActionResult<ItemStack> onItemLeftClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+        boolean flag = !this.findAmmo2(playerIn).isEmpty();
+
+        ActionResult<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onArrowNock(itemstack, worldIn, playerIn, handIn, flag);
+        if (ret != null) return ret;
+
+        if (!playerIn.capabilities.isCreativeMode && !flag) {
+            return flag ? new ActionResult(EnumActionResult.PASS, itemstack) : new ActionResult(EnumActionResult.FAIL, itemstack);
+        }
+        else {
+            playerIn.setActiveHand(handIn);
+            return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
+        }
+    }
+
+
     @Override
     public void registerModels() {
         Main.proxy.registerItemRenderer(this, 0, "inventory");
     }
-
-
-    //    public static float getArrowVelocity(int charge) //charge: ticks that right click has been held down
-//    {
-//        float f = (float)charge / 20.0F;
-//        f = (f * f + f * 2.0F) / 3.0F;
-//        if (f > 1.0F) {
-//            f = 1.0F;
-//        }
-//        return f;
-//    }
 }
