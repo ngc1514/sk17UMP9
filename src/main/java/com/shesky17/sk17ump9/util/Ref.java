1a1ade05daf778cc1f9b94274c997db8fae403dc
Shesky17 <aidenx58@gmail.com>
2020-01-23 00:09:08 -0500

    Updated version number

--- src/main/java/com/shesky17/sk17ump9/util/Ref.java
+++ src/main/java/com/shesky17/sk17ump9/util/Ref.java
@@ -6,10 +6,11 @@ package com.shesky17.sk17ump9.util;
 public class Ref {
     public static final String MOD_ID = "sk17ump9";
     public static final String NAME = "I Love UMP9 Mod";
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.0.3";
     public static final String ACCEPTED_VERSION = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "com.shesky17.sk17ump9.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.shesky17.sk17ump9.proxy.CommonProxy";
 
+    //some id for 9mm entity
     public static final int ENTITY_9MM = 250;
 }
